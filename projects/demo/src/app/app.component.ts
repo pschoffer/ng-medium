8d91886a7c8dc703397161624ff0c7464cb6e41d
Pavel Schoffer <xschof00@gmail.com>
2020-01-13 07:37:19 +0100

    making demo more fun

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'demo';
+  @ViewChild("feedInput", { static: false }) feedInput: ElementRef;
+  feedUrl = "https://medium.com/feed/tag/angular-4";
+
+  updateFeed() {
+    this.feedUrl = this.feedInput.nativeElement.value;
+  }
 }
